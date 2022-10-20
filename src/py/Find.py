938ae86eb4e0565b77ba2affe57e1e01f915b767
charviis <marekgta233@gmail.com>
2022-10-20 22:27:48 +0200

    visuals

---
+++ src/py/Find.py
@@ -0,0 +1,32 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+from collections import defaultdict
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=os.environ["SPOTIFY_CLIENT_ID"],
+                                                           client_secret=os.environ["SPOTIFY_CLIENT_SECRET"]))
+
+
+def find_song(name, year):
+    
+    song_data = defaultdict()
+    results = sp.search(q= 'track: {} year: {}'.format(name,
+                                                       year), limit=1)
+    if results['tracks']['items'] == []:
+        return None
+    
+    results = results['tracks']['items'][0]
+
+    track_id = results['id']
+    audio_features = sp.audio_features(track_id)[0]
+    
+    song_data['name'] = [name]
+    song_data['year'] = [year]
+    song_data['explicit'] = [int(results['explicit'])]
+    song_data['duration_ms'] = [results['duration_ms']]
+    song_data['popularity'] = [results['popularity']]
+    
+    for key, value in audio_features.items():
+        song_data[key] = value
+    
+    return pd.DataFrame(song_data)
+
+    
